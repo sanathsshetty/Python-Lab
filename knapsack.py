4c1ce893e3ddc2f6d1c453743370e81d599c2653
sanathsshetty <120259170+sanathsshetty@users.noreply.github.com>
2023-08-17 09:40:29 +0530

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -13,7 +13,7 @@ def knapsack_max_profit(weights,costs,capacity):
   for i in range(num_items,0,-1):
     if table[i][total_weight]!=table[i-1][total_weight]:
       selected_items.append(i-1)
-      total_weight==weights[i-1]
+      total_weight-=weights[i-1]
   return table[num_items][capacity],selected_items
 # weights=[2,3,4,5]     (lines 18,19,20 are used only if given in question)
 # costs=[10,20,30,40]       (lines 21-25 used for user input only)
