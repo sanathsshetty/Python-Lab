18e2004831e56ccffb8c11a2106d530c162241a6
sanathsshetty <120259170+sanathsshetty@users.noreply.github.com>
2023-09-08 14:25:15 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,9 @@
+for i in range(0, 3):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+for i in range(3, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
